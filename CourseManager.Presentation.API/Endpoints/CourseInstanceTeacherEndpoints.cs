248ac994ec5b39d231ebf9151404f802d25bad2b
Emil L <emillarsson99@gmail.com>
2026-02-21 16:21:54 +0100

    Created CRUD for CourseInstanceTeacher (assign/unassign) entity

---
+++ CourseManager.Presentation.API/Endpoints/CourseInstanceTeacherEndpoints.cs
@@ -0,0 +1,37 @@
+﻿using CourseManager.Application.Features.CourseInstanceTeacher;
+
+namespace CourseManager.Presentation.API.Endpoints;
+
+public static class CourseInstanceTeacherEndpoints
+{
+    public static IEndpointRouteBuilder MapCourseInstanceTeachersEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("/course-instances/{courseInstanceId:guid}/teachers/AssignTeacher", async (
+            Guid courseInstanceId, 
+            Guid teacherId, 
+            CourseInstanceTeacherService service, 
+            CancellationToken ct) =>
+        {
+            await service.AssignTeacher(courseInstanceId, teacherId, ct);
+            return Results.NoContent();
+        });
+
+        app.MapDelete("/course-instances/{courseInstanceId:guid}/teachers/UnassignTeacher", async (
+            Guid courseInstanceId,
+            Guid teacherId, 
+            CourseInstanceTeacherService service, 
+            CancellationToken ct) =>
+        {
+            await service.UnassignTeacher(courseInstanceId, teacherId, ct);
+            return Results.NoContent();
+        });
+
+        app.MapGet("/course-instances/{courseInstanceId:guid}/teachers", async (Guid courseInstanceId, CourseInstanceTeacherService service, CancellationToken ct) =>
+        {
+            var teacherIds = await service.ListTeacherIdAsync(courseInstanceId, ct);
+            return Results.Ok(teacherIds);
+        });
+
+        return app;
+    }
+}
